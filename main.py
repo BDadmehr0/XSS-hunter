dafb2ac073b735e6129e2397e7e778f6548e77b9
Bill <134191240+BDadmehr0@users.noreply.github.com>
2023-08-22 19:05:56 +0330

    full

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import platform
+from colorama import Fore as F
 
-syss = platform.system()
-print(syss)
+def system_guard():
+    sys = platform.system()
+    if sys == 'Linux':
+        print(F.GREEN+'\nWelcome To XSS-Hunter.\n')
+    else:
+        print(F.RED+f'\nScript Not Support Your System: {sys}\n')
+
+if __name__ == '__main__':
+    system_guard()
