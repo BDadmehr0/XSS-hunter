0e172b493005ad8ff1790dc3e5a239e29b234ea4
Bill <134191240+BDadmehr0@users.noreply.github.com>
2023-08-22 19:11:37 +0330

    Banner

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import platform
 from colorama import Fore as F
 
+def banner():
+    ascii_b = f'''
+__  _____ ___     _  _          _           
+\ \/ / __/ __|___| || |_  _ _ _| |_ ___ _ _  Find XSS Bugs
+ >  <\__ \__ \___| __ | || | ' \  _/ -_) '_| github.com/BDadmehr0
+/_/\_\___/___/   |_||_|\_,_|_||_\__\___|_|   V1.0.0'''
+
 def system_guard():
     sys = platform.system()
     if sys == 'Linux':
