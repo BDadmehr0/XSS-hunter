7fadc3b10f3315c26a7004e13310662a2069724b
Bill <134191240+BDadmehr0@users.noreply.github.com>
2023-08-22 19:14:27 +0330

    full

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from colorama import Fore as F
 
 def banner():
     ascii_b = f'''
-__  _____ ___     _  _          _           
-\ \/ / __/ __|___| || |_  _ _ _| |_ ___ _ _  Find XSS Bugs
- >  <\__ \__ \___| __ | || | ' \  _/ -_) '_| github.com/BDadmehr0
-/_/\_\___/___/   |_||_|\_,_|_||_\__\___|_|   V1.0.0'''
+ __  _____ ___     _  _          _           
+ \ \/ / __/ __|___| || |_  _ _ _| |_ ___ _ _  Find XSS Bugs
+  >  <\__ \__ \___| __ | || | ' \  _/ -_) '_| github.com/BDadmehr0
+ /_/\_\___/___/   |_||_|\_,_|_||_\__\___|_|   V1.0.0'''
+    print(ascii_b)
 
 def system_guard():
     sys = platform.system()
@@ -16,4 +17,5 @@ def system_guard():
         print(F.RED+f'\nScript Not Support Your System: {sys}\n')
 
 if __name__ == '__main__':
+    banner()
     system_guard()
