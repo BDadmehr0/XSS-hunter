fd804f6a2ca2b454a6cdcf860d39da04a2ef1e5d
Bill <134191240+BDadmehr0@users.noreply.github.com>
2023-08-23 03:02:06 +0330

    readline file XSS-Last.txt Error

--- core/process.py
+++ core/process.py
@@ -1,4 +1,16 @@
+import requests
 
+class main:
 
-def attack(input_b):
-    return input_b
+    def read_payload():
+        file_path = "XSS-Latest.txt"
+
+        with open(file_path, "r") as file1:
+            Lines = file1.readlines()
+            for line in Lines:
+                return Lines
+                 
+
+     
+
+print(main.read_payload())
